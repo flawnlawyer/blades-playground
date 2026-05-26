acd9eed5a421e9cf484eb8e09fae644479f92fec
flawnlawyer <ojhaayush497@gmail.com>
2026-05-26 08:03:27 +0545

    feat: add __init__.py to define game state classes for better organization

---
+++ game/states/__init__.py
@@ -0,0 +1,8 @@
+"""game.states — All game state classes."""
+from game.states.loading_state    import LoadingState
+from game.states.menu_state       import MenuState
+from game.states.play_state       import PlayState
+from game.states.pause_state      import PauseState
+from game.states.gameover_state   import GameOverState
+from game.states.highscores_state import HighScoresState
+from game.states.settings_state   import SettingsState
